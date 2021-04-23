9b85f5f687abb86115f586bb04af650e7b9ec083
allamesed6@gmail.com <allamesed6@gmail.com>
2021-04-23 05:20:01 +0300

    Add project files.

---
+++ MyDictionary/MyDictionaryClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionaryClass<K, V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionaryClass()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempKeys = keys;
+            keys = new K[keys.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+            V[] tempValues = values;
+            values = new V[values.Length + 1];
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            values[values.Length - 1] = value;
+        }
+        public int keysLength
+        {
+            get
+            {
+                return keys.Length;
+            }
+        }
+        public int valuesLength
+        {
+            get
+            {
+                return values.Length;
+            }
+        }
+        public K[] Keys
+        {
+            get
+            {
+                return keys;
+            }
+        }
+        public V[] Values
+        {
+            get
+            {
+                return values;
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryClass<int, string> genus = new MyDictionaryClass<int, string>();
+            genus.Add(1, "Scottish");
+            genus.Add(2, "British");
+            genus.Add(3, "Sfenks");
+            genus.Add(4, "Himalaya");
+            genus.Add(5, "İran");
+
+
+            //Console.WriteLine(dictionary1.keysLength);
+
+            for (int i = 0; i < genus.keysLength; i++)
+            {
+                Console.WriteLine("Cins türü : " + genus.Values[i] + "                  cins kodu : " + genus.Keys[i]);
+            }
+        }
+    }
+}
